8f306e4efa9cfb43aad0ab16ce565fe01f8f1e85
Michael Arnaldi <arnaldimichael@gmail.com>
2022-06-18 15:37:24 +0000

    chore: improve contentlayer config

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -3,56 +3,11 @@ import highlight from 'rehype-highlight'
 import remarkShikiTwoslash from 'remark-shiki-twoslash'
 import rehypeRaw from 'rehype-raw'
 import { nodeTypes } from '@mdx-js/mdx'
-
-export const Doc = defineDocumentType(() => ({
-  name: 'Doc',
-  filePathPattern: `**/*.md`,
-  contentType: 'mdx',
-  fields: {
-    title: {
-      type: 'string',
-      description: 'The title of the page',
-      required: true,
-    },
-    show_child_cards: {
-      type: 'boolean',
-      default: false,
-    },
-    nav_title: {
-      type: 'string',
-      description: 'Override the title for display in nav',
-      required: false,
-    },
-    label: {
-      type: 'string',
-      required: false,
-    },
-    excerpt: {
-      type: 'string',
-      required: false,
-    },
-  },
-  computedFields: {
-    pathSegments: {
-      type: 'json',
-      resolve: (doc) =>
-        doc._raw.flattenedPath
-          .split('/')
-          .slice(0)
-          .map((dirName) => {
-            const re = /^((\d+)-)?(.*)$/
-            const [, , orderStr, pathName] = dirName.match(re) ?? []
-            const order = orderStr ? parseInt(orderStr) : 0
-            return { order, pathName }
-          }),
-    },
-  },
-  extensions: {},
-}))
+import * as DT from './documentTypes'
 
 export default makeSource({
-  contentDirPath: './docs',
-  documentTypes: [Doc],
+  contentDirPath: './content',
+  documentTypes: Object.values(DT),
   mdx: {
     rehypePlugins: [highlight, [rehypeRaw, { passThrough: nodeTypes }]],
     // @ts-expect-error
