fba315cc14d88c3f1c42dadc86a747927fd7bd0f
Michael Arnaldi <arnaldimichael@gmail.com>
2022-06-27 18:15:18 +0000

    chore: switch to console.log

--- todo-fetcher/src/main.ts
+++ todo-fetcher/src/main.ts
@@ -1,8 +1,11 @@
 import * as Effect from "@effect/core/io/Effect";
 import * as Layer from "@effect/core/io/Layer";
-import { pipe } from "@tsplus/stdlib/data/Function";
+import { LazyArg, pipe } from "@tsplus/stdlib/data/Function";
 import { HttpServiceLive } from "./http.js";
 import { TodoRepo, TodoRepoLive } from "./todos.js";
+
+const log = (message: LazyArg<string>) =>
+  Effect.succeed(() => console.log(message()));
 
 const main = Effect.gen(function* ($) {
   const { getTodosBetween } = yield* $(TodoRepo);
@@ -10,7 +13,7 @@ const main = Effect.gen(function* ($) {
   const todos = yield* $(getTodosBetween(1, 10));
 
   for (const todo of todos) {
-    yield* $(Effect.log(() => `Todo: ${JSON.stringify(todo)}`));
+    yield* $(log(() => `Todo: ${JSON.stringify(todo)}`));
   }
 });
 
