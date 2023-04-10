ee9da8216f18b80e55ce79566119b066e71a847b
Michael Arnaldi <ma@matechs.com>
2023-04-10 16:46:42 +0000

    fix discord link

--- theme.config.jsx
+++ theme.config.jsx
@@ -48,7 +48,7 @@ export default {
     link: "https://github.com/Effect-TS/effect",
   },
   chat: {
-    link: "https://discord.gg/RVZKYxWfAJ",
+    link: "https://discord.gg/effect-ts",
   },
   footer: {
     text: (
