f437d40fd277337770a5583eeeb680fd2fe9b054
lkb <tmwlak@gmail.com>
2023-04-12 14:23:24 +0900

    useCustomStyle added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
+import {useCustomStyle} from '@/hooks/useCustomStyle'
 
 export default function App({ Component, pageProps }: AppProps) {
+  
+  useCustomStyle()
+
   return <Component {...pageProps} />
 }
