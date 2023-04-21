237ca19aff17f99c318de523853994ef26c84f44
lee gukbeom <tmwlak@gmail.com>
2023-04-21 18:47:05 +0900

    prettier formatteed

--- components/TwoslashPatch.tsx
+++ components/TwoslashPatch.tsx
@@ -1,26 +1,22 @@
 import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
-import {useCustomStyle} from '@/hooks/useCustomStyle'
+import { useCustomStyle } from "@/hooks/useCustomStyle";
 
 export default function TwoslashPatchPortal() {
-
   const [isClientReady, setIsClientReady] = useState(false);
 
-    useEffect(() => {
-        setIsClientReady(true);    
-      },[])
+  useEffect(() => {
+    setIsClientReady(true);
+  }, []);
 
-    const {
-        // isClientReady,
-        currentStyle
-    } = useCustomStyle()
+  const {
+    currentStyle,
+  } = useCustomStyle();
 
-    return  <>
-        {isClientReady &&
-        createPortal(
-            <style>
-                {currentStyle}
-            </style>
-        , document.body)}
+  return (
+    <>
+      {isClientReady &&
+        createPortal(<style>{currentStyle}</style>, document.body)}
     </>
+  );
 }
--- hooks/useCustomStyle.tsx
+++ hooks/useCustomStyle.tsx
@@ -1,137 +1,101 @@
 import { useEffect, useState } from "react";
 
-
 function getOneSidePadding(mainElement: HTMLElement) {
-
   return Number(getComputedStyle(mainElement).paddingLeft.replace("px", ""));
 }
 
-function getPseudoElementWidth(currentElement: HTMLElement, pseudoName: string) {
-
-  return Number(getComputedStyle(currentElement, pseudoName).width.replace('px', ''))
+function getPseudoElementWidth(
+  currentElement: HTMLElement,
+  pseudoName: string
+) {
+  return Number(
+    getComputedStyle(currentElement, pseudoName).width.replace("px", "")
+  );
 }
 
 function isInCodeContainer(currentElement: HTMLElement) {
-  const containerElement = currentElement.closest('.code-container')!
-  return containerElement?.scrollLeft
+  const containerElement = currentElement.closest(".code-container")!;
+  return containerElement?.scrollLeft;
 }
 
 function getValuesForCSSLeft(currentElement: HTMLElement) {
-
   const screenWidth = window.innerWidth;
-  const mainElement = document.querySelector('main')!
-  const mainLeftPadding = getOneSidePadding(mainElement)
+  const mainElement = document.querySelector("main")!;
+  const mainLeftPadding = getOneSidePadding(mainElement);
+  const allowedContentFullWidth = screenWidth - mainLeftPadding * 2;
 
-  const allowedContentFullWidth = screenWidth - (mainLeftPadding * 2)
-
-  const elementXPos = currentElement.getBoundingClientRect().x - mainLeftPadding
-  const elementXPosRatio = elementXPos / allowedContentFullWidth
+  const elementXPos =
+    currentElement.getBoundingClientRect().x - mainLeftPadding;
+  const elementXPosRatio = elementXPos / allowedContentFullWidth;
 
   return {
     elementXPos,
     allowedContentFullWidth,
     elementXPosRatio,
-  }
+  };
 }
 
 function computeTwoSlashOverrideStyle({
-  // isMounted,
   contentWidth,
   allowedContentFullWidth,
   elementXPos,
-}:
-  {
-    // isMounted: boolean,
-    contentWidth: number,
-    allowedContentFullWidth: number,
-    elementXPos: number,
-  }) {
-
-
-                        return contentWidth >= allowedContentFullWidth
+}: {
+  contentWidth: number;
+  allowedContentFullWidth: number;
+  elementXPos: number;
+}) {
+  return contentWidth >= allowedContentFullWidth
     ? `pre.twoslash data-lsp:hover::before {
     left: 0px !important;
     }`
     : elementXPos + contentWidth >= allowedContentFullWidth
-      ? `pre.twoslash data-lsp:hover::before {
+    ? `pre.twoslash data-lsp:hover::before {
     left: ${allowedContentFullWidth - contentWidth}px !important;
     }`
-      : `pre.twoslash data-lsp:hover::before {
+    : `pre.twoslash data-lsp:hover::before {
     left: ${elementXPos}px !important;
-    }`
-
-  // return  isMounted
-  //         ?(
-  //             contentWidth >= allowedContentFullWidth
-  //             ? `pre.twoslash data-lsp:hover::before {
-  //             left: 0px !important;
-  //             }`
-  //             : elementXPos + contentWidth >= allowedContentFullWidth
-  //             ? `pre.twoslash data-lsp:hover::before {
-  //             left: ${allowedContentFullWidth - contentWidth}px !important;
-  //             }`
-  //             : `pre.twoslash data-lsp:hover::before {
-  //             left: ${elementXPos}px !important;
-  //             }`
-  //         )
-  //         : "";
+    }`;
 }
 
-
 export function useCustomStyle() {
-
   const [currentStyle, setCurrentStyle] = useState("");
 
   const rePositionTwoSlashBox = (e: Event) => {
-    const isMobile = globalThis.matchMedia("(max-width: 767px)").matches
+    const isMobile = globalThis.matchMedia("(max-width: 767px)").matches;
+    const currentElement = e.target as HTMLElement | null;
+    if (currentElement == null || !isMobile) return;
+    if (isInCodeContainer(currentElement) == null) return;
 
-    const currentElement = e.target as (HTMLElement | null)
-    if (currentElement == null || !isMobile) return
-    if (isInCodeContainer(currentElement) == null) return
+    const { elementXPos, allowedContentFullWidth } =
+      getValuesForCSSLeft(currentElement);
 
-    const {
-      elementXPos,
-      allowedContentFullWidth,
-    } = getValuesForCSSLeft(currentElement)
-
-    const contentWidth = getPseudoElementWidth(currentElement, ':before');
+    const contentWidth = getPseudoElementWidth(currentElement, ":before");
 
     setCurrentStyle(
       computeTwoSlashOverrideStyle({
-        // isMounted: isClientReady,
         contentWidth,
         allowedContentFullWidth,
         elementXPos,
       })
-    )
-  }
-
+    );
+  };
 
   useEffect(() => {
-
     const touchAndClickListener = (e: Event) => {
-      e.preventDefault()
-      rePositionTwoSlashBox(e)
-    }
+      e.preventDefault();
+      rePositionTwoSlashBox(e);
+    };
 
-    window.addEventListener("touch", touchAndClickListener)
-    window.addEventListener("click", touchAndClickListener)
+    window.addEventListener("touch", touchAndClickListener);
+    window.addEventListener("click", touchAndClickListener);
 
     return () => {
-      window.removeEventListener("touch", touchAndClickListener)
-      window.removeEventListener("click", touchAndClickListener)
-    }
+      window.removeEventListener("touch", touchAndClickListener);
+      window.removeEventListener("click", touchAndClickListener);
+    };
   }, []);
-
-  //   useEffect(() => {
-  //     setIsClientReady(true);
-  //     return () => setIsClientReady(false);
-
-  //   },[])
-
 
   return {
     currentStyle,
-    // isClientReady ,
-  }
+  };
 }
