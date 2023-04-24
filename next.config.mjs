48054fa3bc791b688e9b3ff675b2dbd0d8c2849a
Jesse Kelly <kellyjesse881@gmail.com>
2023-04-24 12:25:39 -0500

    feat: migrated docs from @effect/data (#32)
    Co-authored-by: Maxwell Brown <maxwellbrown1990@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -2,20 +2,25 @@ import { nodeTypes } from '@mdx-js/mdx';
 import nextra from "nextra";
 import rehypeRaw from "rehype-raw";
 import remarkShikiTwoslash from "remark-shiki-twoslash";
+import { remarkMermaid } from 'remark-mermaid-nextra';
 
 const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
   mdxOptions: {
     rehypePlugins: [[rehypeRaw, { passThrough: nodeTypes }]],
-    remarkPlugins: [[
-      remarkShikiTwoslash.default,
-      {
-        defaultCompilerOptions: {
-          types: ['node'],
-        },
-        themes: ["dark-plus", "light-plus"],
-      }]],
+    remarkPlugins: [
+      remarkMermaid,
+      [
+        remarkShikiTwoslash.default,
+        {
+          defaultCompilerOptions: {
+            types: ['node'],
+          },
+          themes: ["dark-plus", "light-plus"],
+        }
+      ]
+    ],
   }
 });
 
