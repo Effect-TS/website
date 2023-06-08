5adcbebeccfd3b4b62753b9b7ff96d269966828e
gcanti <giulio.canti@gmail.com>
2023-06-08 17:10:40 +0200

    add categories

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,28 @@
-import { nodeTypes } from "@mdx-js/mdx"
+// import { nodeTypes } from "@mdx-js/mdx"
 import nextra from "nextra"
-import rehypeRaw from "rehype-raw"
-import remarkShikiTwoslash from "remark-shiki-twoslash"
-import { remarkMermaid } from "remark-mermaid-nextra"
+// import rehypeRaw from "rehype-raw"
+// import remarkShikiTwoslash from "remark-shiki-twoslash"
+// import { remarkMermaid } from "remark-mermaid-nextra"
 
 const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
-  mdxOptions: {
-    rehypePlugins: [[rehypeRaw, { passThrough: nodeTypes }]],
-    remarkPlugins: [
-      remarkMermaid,
-      [
-        remarkShikiTwoslash.default,
-        {
-          defaultCompilerOptions: {
-            types: ["node"],
-          },
-          themes: ["dark-plus", "light-plus"],
-        },
-      ],
-    ],
-  },
+  defaultShowCopyCode: true,
+  // mdxOptions: {
+  //   rehypePlugins: [[rehypeRaw, { passThrough: nodeTypes }]],
+  //   remarkPlugins: [
+  //     remarkMermaid,
+  //     [
+  //       remarkShikiTwoslash.default,
+  //       {
+  //         defaultCompilerOptions: {
+  //           types: ["node"],
+  //         },
+  //         themes: ["dark-plus", "light-plus"],
+  //       },
+  //     ],
+  //   ],
+  // },
 })
 
 /** @type {import('next').NextConfig} */
