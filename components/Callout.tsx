56b4ba4fb0a0830022a6470fe8ebac9dea0d138e
Giulio Canti <giulio.canti@gmail.com>
2023-06-13 09:52:48 +0200

    add categories (#66)

---
+++ components/Callout.tsx
@@ -0,0 +1,22 @@
+import { Callout } from "nextra-theme-docs"
+import type { ReactNode } from "react"
+
+export function Error({ children }: { children: ReactNode }) {
+  return <Callout type="error">{children}</Callout>
+}
+
+export function Info({ children }: { children: ReactNode }) {
+  return <Callout type="info">{children}</Callout>
+}
+
+export function Warning({ children }: { children: ReactNode }) {
+  return <Callout type="warning">{children}</Callout>
+}
+
+export function Design({ children }: { children: ReactNode }) {
+  return <Callout emoji="🎨">{children}</Callout>
+}
+
+export function Idea({ children }: { children: ReactNode }) {
+  return <Callout>{children}</Callout>
+}
