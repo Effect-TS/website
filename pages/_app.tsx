36c62120d762e9709db99ff5ebe4cb229277b35d
gcanti <giulio.canti@gmail.com>
2023-06-22 15:05:46 +0200

    chore

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
 // import {useCustomStyle} from '@/hooks/useCustomStyle'
-import TwoslashPatch from "@/components/TwoslashPatch"
+// import TwoslashPatch from "@/components/TwoslashPatch"
 
 export default function App({ Component, pageProps }: AppProps) {
   // useCustomStyle()
 
   return (
     <>
-      <TwoslashPatch />
+      {/* <TwoslashPatch /> */}
       <Component {...pageProps} />
     </>
   )
