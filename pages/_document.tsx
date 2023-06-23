f6756276640993bf64527d3a4b5151279d843ecf
Johannes Schickling <schickling.j@gmail.com>
2023-06-23 16:31:18 +0200

    twitter sharing image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,22 @@
 import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
+  // Netlify URL https://docs.netlify.com/configure-builds/environment-variables/#deploy-urls-and-metadata
+  const deployUrl = process.env.DEPLOY_URL
+    ? "https://" + process.env.DEPLOY_URL
+    : "https://effect.website"
+
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="og:title" content="Vercel Edge Network" />
+        <meta name="og:description" content="Vercel Edge Network" />
+        <meta
+          name="og:image"
+          // NOTE og:image must be an absolute url
+          content={`${deployUrl}/twitter.png`}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
