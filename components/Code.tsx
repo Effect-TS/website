e21b35b9a4f1c41e143a729d59525c50e3968522
gcanti <giulio.canti@gmail.com>
2023-06-23 12:10:25 +0200

    refactor generators

--- components/Code.tsx
+++ components/Code.tsx
@@ -1,8 +1,18 @@
 import { Tabs as T, Tab } from "nextra-theme-docs"
 import type { ReactNode } from "react"
 
-export function Tabs({ children }: { children: ReactNode }) {
-  return <T items={["Using pipe", "Using Effect.gen"]}>{children}</T>
+export function Tabs({
+  defaultIndex,
+  children,
+}: {
+  defaultIndex?: number
+  children: ReactNode
+}) {
+  return (
+    <T defaultIndex={defaultIndex} items={["Using pipe", "Using Effect.gen"]}>
+      {children}
+    </T>
+  )
 }
 
 export { Tab }
