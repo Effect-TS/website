b6dbbb5ea852d5aa51db8abdd85d19cb300cbfc4
Giulio Canti <giulio.canti@gmail.com>
2023-06-26 14:54:48 +0200

    add remark-gfm plugin (#124)

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 // import { nodeTypes } from "@mdx-js/mdx"
 import nextra from "nextra"
+import remarkGFM from "remark-gfm"
 // import rehypeRaw from "rehype-raw"
 // import remarkShikiTwoslash from "remark-shiki-twoslash"
 
@@ -7,20 +8,20 @@ const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   defaultShowCopyCode: true,
-  // mdxOptions: {
-  //   rehypePlugins: [[rehypeRaw, { passThrough: nodeTypes }]],
-  //   remarkPlugins: [
-  //     [
-  //       remarkShikiTwoslash.default,
-  //       {
-  //         defaultCompilerOptions: {
-  //           types: ["node"],
-  //         },
-  //         themes: ["dark-plus", "light-plus"],
-  //       },
-  //     ],
-  //   ],
-  // },
+  mdxOptions: {
+    // rehypePlugins: [[rehypeRaw, { passThrough: nodeTypes }]],
+    remarkPlugins: [remarkGFM,
+      // [
+      //   remarkShikiTwoslash.default,
+      //   {
+      //     defaultCompilerOptions: {
+      //       types: ["node"],
+      //     },
+      //     themes: ["dark-plus", "light-plus"],
+      //   },
+      // ],
+    ],
+  },
 })
 
 /** @type {import('next').NextConfig} */
