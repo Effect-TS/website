68000f3c8cef8e733e931488cebed71d8a5a8941
gcanti <giulio.canti@gmail.com>
2023-07-15 12:25:47 +0200

    refactor essentials/exit

---
+++ src/essentials/exit/matching.ts
@@ -0,0 +1,21 @@
+import { Effect, Exit } from "effect"
+
+// Exit<never, number>
+const simulatedSuccess = Effect.runSyncExit(Effect.succeed(1))
+
+Exit.match(simulatedSuccess, {
+  onFailure: (cause) =>
+    console.error(`Exited with failure state: ${cause._tag}`),
+  onSuccess: (value) => console.log(`Exited with success value: ${value}`),
+})
+// Output: "Exited with success value: 1"
+
+// Exit<string, never>
+const simulatedFailure = Effect.runSyncExit(Effect.fail("error"))
+
+Exit.match(simulatedFailure, {
+  onFailure: (cause) =>
+    console.error(`Exited with failure state: ${cause._tag}`),
+  onSuccess: (value) => console.log(`Exited with success value: ${value}`),
+})
+// Output: "Exited with failure state: Fail"
