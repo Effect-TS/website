42c7e1e90cb5adc8295e67bf787721def0acdd1e
gcanti <giulio.canti@gmail.com>
2023-07-16 22:29:36 +0200

    guide/observability/custom-logger

---
+++ src/guide/observability/custom-logger/program.ts
@@ -0,0 +1,18 @@
+import { Effect } from "effect"
+
+// $ExpectType Effect<never, never, void>
+const task1 = Effect.sleep("2 seconds").pipe(
+  Effect.flatMap(() => Effect.log("task1 done", { level: "Debug" }))
+)
+
+// $ExpectType Effect<never, never, void>
+const task2 = Effect.sleep("1 seconds").pipe(
+  Effect.flatMap(() => Effect.log("task2 done", { level: "Debug" }))
+)
+
+// $ExpectType Effect<never, never, void>
+export const program = Effect.log("start").pipe(
+  Effect.flatMap(() => task1),
+  Effect.flatMap(() => task2),
+  Effect.flatMap(() => Effect.log("done"))
+)
