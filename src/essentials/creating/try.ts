a1e5e3a27ac0ba0a57211450f13f5dac1b24b2be
Diogo Korok <korokd@protonmail.ch>
2023-07-19 15:03:36 -0300

    Update `try.ts` to use the `try` function
    The code example for the simple use case of the `try` function uses the `sync` function. This fixes that.

--- src/essentials/creating/try.ts
+++ src/essentials/creating/try.ts
@@ -1,7 +1,7 @@
 import { Effect } from "effect"
 
 // $ExpectType Effect<never, never, number>
-const program = Effect.sync(() => {
+const program = Effect.try(() => {
   console.log("Hello, World!") // side effect
   return 42 // return value
 })
