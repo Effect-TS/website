754f7d040986b6b8deaafc59731deb3d5d7a40be
gcanti <giulio.canti@gmail.com>
2023-07-27 19:35:24 +0200

    refactor Configuration

--- src/guide/configuration/withDefault-pipe.ts
+++ src/guide/configuration/withDefault-pipe.ts
@@ -1,12 +1,12 @@
 import { Effect, Config } from "effect"
 
-const program = Effect.all([
-  Effect.config(Config.string("HOST")),
-  Effect.config(Config.withDefault(Config.number("PORT"), 8080)),
-]).pipe(
-  Effect.flatMap(([host, port]) =>
+const program = Effect.flatMap(
+  Effect.all([
+    Effect.config(Config.string("HOST")),
+    Effect.config(Config.withDefault(Config.number("PORT"), 8080)),
+  ]),
+  ([host, port]) =>
     Effect.sync(() => console.log(`Application started: ${host}:${port}`))
-  )
 )
 
 Effect.runSync(program)
