df08c87d260fa9aa275da62e42dc036575d41102
gcanti <giulio.canti@gmail.com>
2023-07-27 18:53:59 +0200

    refactor Resource Management

--- src/guide/resource-management/scope/scoped-pipe.ts
+++ src/guide/resource-management/scope/scoped-pipe.ts
@@ -2,9 +2,8 @@ import { Effect } from "effect"
 import { resource } from "./resource"
 
 // $ExpectType Effect<never, Error, void>
-const program = resource.pipe(
-  Effect.flatMap((res) =>
+const program = Effect.scoped(
+  Effect.flatMap(resource, (res) =>
     Effect.sync(() => console.log(`content is ${res.contents}`))
-  ),
-  Effect.scoped
+  )
 )
--- src/guide/resource-management/scope/using-pipe.ts
+++ src/guide/resource-management/scope/using-pipe.ts
@@ -2,8 +2,6 @@ import { Effect } from "effect"
 import { resource } from "./resource"
 
 // $ExpectType Effect<Scope, Error, void>
-const program = resource.pipe(
-  Effect.flatMap((res) =>
-    Effect.sync(() => console.log(`content is ${res.contents}`))
-  )
+const program = Effect.flatMap(resource, (res) =>
+  Effect.sync(() => console.log(`content is ${res.contents}`))
 )
