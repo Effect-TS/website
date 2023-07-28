f4dd4c185d0b295500b439ddc6c72201d0b07ac3
gcanti <giulio.canti@gmail.com>
2023-07-28 08:14:05 +0200

    refactor Introduction to Effect's Control Flow Operators

--- src/guide/control-flow/forEach-discard.ts
+++ src/guide/control-flow/forEach-discard.ts
@@ -3,16 +3,20 @@ import { Effect } from "effect"
 // $ExpectType Effect<never, never, void>
 const result = Effect.forEach(
   [1, 2, 3, 4, 5],
-  (index) => Effect.sync(() => console.log(`Currently at index ${index}`)),
+  (n, index) =>
+    Effect.sync(() => {
+      console.log(`Currently at index ${index}`)
+      return n * 2
+    }),
   { discard: true }
 )
 
 console.log(Effect.runSync(result))
 /*
+Currently at index 0
 Currently at index 1
 Currently at index 2
 Currently at index 3
 Currently at index 4
-Currently at index 5
 undefined
 */
