2a8ad7583d2258a0d71753c6fd3a6d08a49cf815
gcanti <giulio.canti@gmail.com>
2023-07-30 12:14:08 +0200

    Custom loggers: chore

--- src/guide/observability/custom-logger/program.ts
+++ src/guide/observability/custom-logger/program.ts
@@ -1,16 +1,13 @@
 import { Effect } from "effect"
 
-// $ExpectType Effect<never, never, void>
 const task1 = Effect.sleep("2 seconds").pipe(
   Effect.flatMap(() => Effect.logDebug("task1 done"))
 )
 
-// $ExpectType Effect<never, never, void>
 const task2 = Effect.sleep("1 seconds").pipe(
   Effect.flatMap(() => Effect.logDebug("task2 done"))
 )
 
-// $ExpectType Effect<never, never, void>
 export const program = Effect.log("start").pipe(
   Effect.flatMap(() => task1),
   Effect.flatMap(() => task2),
