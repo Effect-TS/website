241e1ce7389401c1e562fd0bca94e52b37c7014b
gcanti <giulio.canti@gmail.com>
2023-08-01 12:31:12 +0200

    add Concurrency Options section

---
+++ src/guide/concurrency/concurrency-options/all-unbounded.ts
@@ -0,0 +1,37 @@
+import { Effect, Duration } from "effect"
+
+const makeTask = (n: number, delay: Duration.DurationInput) =>
+  Effect.promise(
+    () =>
+      new Promise<void>((resolve) => {
+        console.log(`start task${n}`)
+        setTimeout(() => {
+          console.log(`task${n} done`)
+          resolve()
+        }, Duration.toMillis(delay))
+      })
+  )
+
+const task1 = makeTask(1, "200 millis")
+const task2 = makeTask(2, "100 millis")
+const task3 = makeTask(3, "210 millis")
+const task4 = makeTask(4, "110 millis")
+const task5 = makeTask(5, "150 millis")
+
+const unbounded = Effect.all([task1, task2, task3, task4, task5], {
+  concurrency: "unbounded",
+})
+
+Effect.runPromise(unbounded)
+/*
+start task1
+start task2
+start task3
+start task4
+start task5
+task2 done
+task4 done
+task5 done
+task1 done
+task3 done
+*/
