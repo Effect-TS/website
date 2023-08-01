c7106e13aa348533c9edda3d1b9616fd77e65e06
Giulio Canti <giulio.canti@gmail.com>
2023-08-01 19:30:41 +0200

    add "Avoiding the Pitfalls of Unknown", closes #224 (#232)

---
+++ src/guide/error-management/expected-errors/error-tracking-solution-with-unknown.ts
@@ -0,0 +1,25 @@
+import { Effect } from "effect"
+
+class GetNumbersError {
+  readonly _tag = "GetNumbersError"
+}
+
+// $ExpectType Effect<never, GetNumbersError, number[]>
+const getNumbers = Effect.tryPromise({
+  try: () => Promise.resolve([1, 2, 3]),
+  catch: () => new GetNumbersError(),
+})
+
+class EmptyError {
+  readonly _tag = "EmptyError"
+}
+
+// $ExpectType Effect<never, GetNumbersError | EmptyError, number>
+const getFirstNumber = Effect.gen(function* (_) {
+  const numbers = yield* _(getNumbers)
+  if (numbers.length > 0) {
+    return numbers[0]
+  } else {
+    return yield* _(Effect.fail(new EmptyError()))
+  }
+})
