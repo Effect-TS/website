e4309288ff7cafc1ce738ff83cf7a160f8e02081
gcanti <giulio.canti@gmail.com>
2023-08-03 15:08:24 +0200

    Resource Management: refactor pattern with layers

--- src/guide/resource-management/pattern/WorkspaceTest-pipe.ts
+++ src/guide/resource-management/pattern/WorkspaceTest-pipe.ts
@@ -1,54 +1,63 @@
-import { Effect, Context } from "effect"
+import { Effect, Context, Layer } from "effect"
 import * as Services from "./Services"
 import * as Workspace from "./Workspace"
 
-const S3Test = (fail: boolean) =>
+type Failure = "S3" | "ElasticSearch" | "Database" | undefined
+
+const Failure = Context.Tag<Failure>()
+
+const S3Test = Layer.function(Failure, Services.S3, (Failure) =>
   Services.S3.of({
     createBucket: Effect.log("[S3] creating bucket").pipe(
       Effect.flatMap(() =>
-        fail
+        Failure === "S3"
           ? Effect.fail(new Services.S3Error())
           : Effect.succeed({ name: "<bucket.name>" })
       )
     ),
     deleteBucket: (bucket) => Effect.log(`[S3] delete bucket ${bucket.name}`),
   })
+)
 
-const ElasticSearchTest = (fail: boolean) =>
-  Services.ElasticSearch.of({
-    createIndex: Effect.log("[ElasticSearch] creating index").pipe(
-      Effect.flatMap(() =>
-        fail
-          ? Effect.fail(new Services.ElasticSearchError())
-          : Effect.succeed({ id: "<index.id>" })
-      )
-    ),
-    deleteIndex: (index) =>
-      Effect.log(`[ElasticSearch] delete index ${index.id}`),
-  })
+const ElasticSearchTest = Layer.function(
+  Failure,
+  Services.ElasticSearch,
+  (Failure) =>
+    Services.ElasticSearch.of({
+      createIndex: Effect.log("[ElasticSearch] creating index").pipe(
+        Effect.flatMap(() =>
+          Failure === "ElasticSearch"
+            ? Effect.fail(new Services.ElasticSearchError())
+            : Effect.succeed({ id: "<index.id>" })
+        )
+      ),
+      deleteIndex: (index) =>
+        Effect.log(`[ElasticSearch] delete index ${index.id}`),
+    })
+)
 
-const DatabaseTest = (fail: boolean) =>
+const DatabaseTest = Layer.function(Failure, Services.Database, (Failure) =>
   Services.Database.of({
     createEntry: (bucket, index) =>
       Effect.log(
         `[Database] creating entry for bucket ${bucket.name} and index ${index.id}`
       ).pipe(
         Effect.flatMap(() =>
-          fail
+          Failure === "Database"
             ? Effect.fail(new Services.DatabaseError())
             : Effect.succeed({ id: "<entry.id>" })
         )
       ),
     deleteEntry: (entry) => Effect.log(`[Database] delete entry ${entry.id}`),
   })
-
-const ctx = Context.empty().pipe(
-  Context.add(Services.S3, S3Test(false)),
-  Context.add(Services.ElasticSearch, ElasticSearchTest(false)),
-  Context.add(Services.Database, DatabaseTest(false))
 )
 
+const layer = Layer.mergeAll(S3Test, ElasticSearchTest, DatabaseTest)
+
 // $ExpectType Effect<never, S3Error | ElasticSearchError | DatabaseError, Entry>
-const runnable = Effect.provideContext(Workspace.make, ctx)
+const runnable = Workspace.make.pipe(
+  Effect.provideLayer(layer),
+  Effect.provideService(Failure, undefined)
+)
 
 Effect.runPromise(Effect.either(runnable)).then(console.log)
