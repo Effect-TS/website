cd9c1c70bbc95e58e77bd8bccf7d22f5bdf83d87
gcanti <giulio.canti@gmail.com>
2023-09-11 12:07:56 +0200

    update data-types/chunk code

--- src/reference/data-types/chunk/appendAll.ts
+++ src/reference/data-types/chunk/appendAll.ts
@@ -6,4 +6,11 @@ const concatenatedChunk = Chunk.appendAll(
   Chunk.make("a", "b")
 )
 
-console.log(concatenatedChunk) // Output: Chunk(1, 2, "a", "b")
+console.log(concatenatedChunk)
+/*
+Output:
+{
+  _id: "Chunk",
+  values: [ 1, 2, "a", "b" ]
+}
+*/
