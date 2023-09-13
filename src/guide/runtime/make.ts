901c6325419888f03546d6ac555f417011f0e4f6
gcanti <giulio.canti@gmail.com>
2023-09-13 08:45:03 +0200

    Providing Context to the Runtime System

---
+++ src/guide/runtime/make.ts
@@ -0,0 +1,45 @@
+import { Effect, Context, Console, Runtime, FiberRefs } from "effect"
+
+interface LoggingService {
+  log: (line: string) => Effect.Effect<never, never, void>
+}
+
+const LoggingService = Context.Tag<LoggingService>()
+
+interface EmailService {
+  send: (user: string, content: string) => Effect.Effect<never, never, void>
+}
+
+const EmailService = Context.Tag<EmailService>()
+
+const LoggingServiceLive = LoggingService.of({
+  log: Console.log
+})
+
+const EmailServiceFake = EmailService.of({
+  send: (user, content) => Console.log(`sending email to ${user}: ${content}`)
+})
+
+// $ExpectType Runtime<LoggingService | EmailService>
+const testableRuntime = Runtime.make({
+  context: Context.make(LoggingService, LoggingServiceLive).pipe(
+    Context.add(EmailService, EmailServiceFake)
+  ),
+  fiberRefs: FiberRefs.unsafeMake(new Map()),
+  runtimeFlags: Runtime.defaultRuntimeFlags
+})
+
+// $ExpectType Effect<LoggingService | EmailService, never, void>
+const program = Effect.gen(function* (_) {
+  const { log } = yield* _(LoggingService)
+  const { send } = yield* _(EmailService)
+  yield* _(log("sending newsletter"))
+  yield* _(send("David", "Hi! Here is today's newsletter."))
+})
+
+Runtime.runPromise(testableRuntime)(program)
+/*
+Output:
+sending newsletter
+sending email to David: Hi! Here is today's newsletter.
+*/
