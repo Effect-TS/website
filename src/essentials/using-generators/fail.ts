c178e78751daf1035f67b4d7f0ccf83e8b1e76b8
Giulio Canti <giulio.canti@gmail.com>
2023-09-15 17:23:59 +0200

    Expand "Using Generators in Effect", closes #299 (#301)

---
+++ src/essentials/using-generators/fail.ts
@@ -0,0 +1,22 @@
+import { Effect } from "effect"
+
+const program = Effect.gen(function* (_) {
+  console.log("Task1...")
+  console.log("Task2...")
+  yield* _(Effect.fail("Something went wrong!"))
+})
+
+Effect.runPromise(program).then(console.log, console.error)
+/*
+Output:
+Task1...
+Task2...
+{
+  _id: "FiberFailure",
+  cause: {
+    _id: "Cause",
+    _tag: "Fail",
+    failure: "Something went wrong!"
+  }
+}
+*/
