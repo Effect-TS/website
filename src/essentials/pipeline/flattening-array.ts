f1e3dabd250bec3f10696b1d0b8fc8c22ad5eeb1
Maxwell Brown <maxwellbrown1990@gmail.com>
2023-11-04 12:45:23 -0400

    incorporate feedback

--- src/essentials/pipeline/flattening-array.ts
+++ src/essentials/pipeline/flattening-array.ts
@@ -1,6 +1,6 @@
 import { pipe, Effect } from "effect"
 
-// Effect.Effect<never, never, Array<number>>
+// $ExpectType Effect<never, never, number[]>
 pipe(
   Effect.succeed([[1,2],[3,4]]),
   Effect.map((arr) => arr.flat())
--- src/essentials/pipeline/flattening-readonlyarray.ts
+++ src/essentials/pipeline/flattening-readonlyarray.ts
@@ -1,6 +1,6 @@
 import { pipe, Effect, ReadonlyArray } from "effect"
 
-// Effect.Effect<never, never, ReadonlyArray<number>>
+// $ExpectType Effect<never, never, readonly number[]>
 pipe(
   Effect.succeed([[1,2],[3,4]]),
   Effect.map(ReadonlyArray.flatten)
