894da227f4a94d9f3fdd023caf344dfb8d608dd1
gcanti <giulio.canti@gmail.com>
2023-11-09 09:39:09 +0100

    Runtime: fix top-level-runtime-configuration example

--- src/guide/runtime/defaultRuntime.ts
+++
@@ -1,7 +0,0 @@
-import { Console, Effect, Runtime } from "effect"
-
-const program = Console.log("Hello World!")
-
-Effect.runSync(program) // Output: Hello World!
-
-Runtime.runSync(Runtime.defaultRuntime)(program) // Output: Hello World!
---
+++ src/runtime/toRuntime.ts
@@ -0,0 +1,30 @@
+import { Logger, Scope, Layer, Effect, Runtime, Exit } from "effect"
+
+// Define a configuration layer
+const appLayer = Logger.replace(
+  Logger.defaultLogger,
+  Logger.make(({ message }) => console.log(message))
+)
+
+// Create a scope for resources used in the configuration layer
+const scope = Effect.runSync(Scope.make())
+
+// Transform the configuration layer into a runtime
+const runtime = Effect.runSync(
+  Layer.toRuntime(appLayer).pipe(Scope.extend(scope))
+)
+
+// Define a custom running function
+const runSync = Runtime.runSync(runtime)
+
+const program = Effect.log("Application started!")
+
+// Execute the program using the custom runtime
+runSync(program)
+/*
+Output:
+Application started!
+*/
+
+// Cleaning up any resources used by the configuration layer
+Effect.runSync(Scope.close(scope, Exit.unit))
