bac610beea8f62e55a8cb5a5afdc047d878f8f15
gcanti <giulio.canti@gmail.com>
2023-12-05 10:37:17 +0100

    add Dependency Memoization

---
+++ src/dependency-memoization/globally-fresh.ts
@@ -0,0 +1,62 @@
+import { Effect, Context, Layer } from "effect"
+
+interface A {
+  readonly a: number
+}
+
+const A = Context.Tag<A>()
+
+interface B {
+  readonly b: string
+}
+
+const B = Context.Tag<B>()
+
+interface C {
+  readonly c: boolean
+}
+
+const C = Context.Tag<C>()
+
+const a = Layer.effect(
+  A,
+  Effect.succeed({ a: 5 }).pipe(Effect.tap(() => Effect.log("initialized")))
+)
+
+const b = Layer.effect(
+  B,
+  Effect.gen(function* (_) {
+    const { a } = yield* _(A)
+    return { b: String(a) }
+  })
+)
+
+const c = Layer.effect(
+  C,
+  Effect.gen(function* (_) {
+    const { a } = yield* _(A)
+    return { c: a > 0 }
+  })
+)
+
+// $ExpectType Effect<B | C, never, void>
+const program = Effect.gen(function* (_) {
+  yield* _(B)
+  yield* _(C)
+})
+
+// $ExpectType Effect<never, never, void>
+const runnable = Effect.provide(
+  program,
+  Layer.merge(
+    Layer.provide(b, Layer.fresh(a)),
+    Layer.provide(c, Layer.fresh(a))
+  )
+)
+
+Effect.runPromise(runnable)
+/*
+Output:
+timestamp=... level=INFO fiber=#2 message=initialized
+timestamp=... level=INFO fiber=#3 message=initialized
+*/
