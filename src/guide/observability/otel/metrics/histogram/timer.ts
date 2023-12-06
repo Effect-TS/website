32ad2fcca81e3be17d31b98de5ce485fc31f2dcb
Giulio Canti <giulio.canti@gmail.com>
2023-12-06 08:53:41 +0100

    update effect (#385)

--- src/guide/observability/otel/metrics/histogram/timer.ts
+++ src/guide/observability/otel/metrics/histogram/timer.ts
@@ -1,7 +1,7 @@
-import { Metric, Chunk, Random, Effect } from "effect"
+import { Metric, ReadonlyArray, Random, Effect } from "effect"
 
 // Metric<Histogram, Duration, Histogram>
-const timer = Metric.timerWithBoundaries("timer", Chunk.range(1, 10))
+const timer = Metric.timerWithBoundaries("timer", ReadonlyArray.range(1, 10))
 
 const program = Random.nextIntBetween(1, 10).pipe(
   Effect.flatMap((n) => Effect.sleep(`${n} millis`)),
@@ -15,23 +15,20 @@ Effect.runPromise(program.pipe(Effect.flatMap(() => Metric.value(timer)))).then(
 /*
 Output:
 HistogramState {
-  buckets: {
-    _id: 'Chunk',
-    values: [
-      [ 1, 3 ],
-      [ 2, 13 ],
-      [ 3, 17 ],
-      [ 4, 26 ],
-      [ 5, 35 ],
-      [ 6, 43 ],
-      [ 7, 53 ],
-      [ 8, 56 ],
-      [ 9, 65 ],
-      [ 10, 72 ],
-      [ Infinity, 100 ],
-      [length]: 11
-    ]
-  },
+  buckets: [
+    [ 1, 3 ],
+    [ 2, 13 ],
+    [ 3, 17 ],
+    [ 4, 26 ],
+    [ 5, 35 ],
+    [ 6, 43 ],
+    [ 7, 53 ],
+    [ 8, 56 ],
+    [ 9, 65 ],
+    [ 10, 72 ],
+    [ Infinity, 100 ],
+    [length]: 11
+  ],
   count: 100,
   min: 0.25797,
   max: 12.25421,
