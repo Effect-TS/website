7733f5a9991030a78b5cd5ccfeb6929c04f219e2
Johannes Schickling <schickling.j@gmail.com>
2023-12-11 13:16:06 +0100

    static docs pages

--- src/app/docs/[...slug]/page.tsx
+++ src/app/docs/[...slug]/page.tsx
@@ -1,14 +1,22 @@
-import {Breadcrumbs} from '@/components/atoms/breadcrumbs'
-import {MDX} from '@/components/atoms/mdx'
-import {ChildCards} from '@/components/docs/child-cards'
-import {Pagination} from '@/components/docs/pagination'
-import {TableOfContents} from '@/components/docs/table-of-contents'
-import {Divider} from '@/components/layout/divider'
-import {getBreadcrumbs} from '@/contentlayer/utils/get-breadcrumbs'
-import {allDocsPages} from 'contentlayer/generated'
-import {notFound} from 'next/navigation'
+import { Breadcrumbs } from '@/components/atoms/breadcrumbs'
+import { MDX } from '@/components/atoms/mdx'
+import { ChildCards } from '@/components/docs/child-cards'
+import { Pagination } from '@/components/docs/pagination'
+import { TableOfContents } from '@/components/docs/table-of-contents'
+import { Divider } from '@/components/layout/divider'
+import { getBreadcrumbs } from '@/contentlayer/utils/get-breadcrumbs'
+import { allDocsPages } from 'contentlayer/generated'
+import { notFound } from 'next/navigation'
 
-export default function Page({params: {slug}}: {params: {slug: string[]}}) {
+export const generateStaticParams = ({ params }: { params: { slug: string[] } }) => {
+  return allDocsPages.map((page) => ({
+    params: {
+      slug: page.urlPath.replace('/docs/', '').split('/'),
+    },
+  }))
+}
+
+export default function Page({ params: { slug } }: { params: { slug: string[] } }) {
   const page = allDocsPages.find((page) => page.urlPath === `/docs/${slug.join('/')}`)
   if (!page) notFound()
   const breadcrumbs = getBreadcrumbs(page.pathSegments)
