b51e8f4a6f6b5443ed8b1535adc9ec9cc61b64c5
Lukas Wiesehan <mail@lukaswiesehan.de>
2023-12-13 13:21:23 +0100

    Style inline code

--- src/components/docs/table-of-contents.tsx
+++ src/components/docs/table-of-contents.tsx
@@ -1,23 +1,23 @@
-'use client'
+"use client"
 
-import {getNodeText, sluggifyTitle} from '@/contentlayer/utils/sluggify'
-import Link from 'next/link'
-import {FC, useEffect, useState} from 'react'
-import {Icon} from '../icons'
-import {Divider} from '../layout/divider'
+import { getNodeText, sluggifyTitle } from "@/contentlayer/utils/sluggify"
+import Link from "next/link"
+import { FC, useEffect, useState } from "react"
+import { Icon } from "../icons"
+import { Divider } from "../layout/divider"
 
-export const TableOfContents: FC<{elements: {level: number; title: string}[]; pageFilePath?: string; pageTitle?: string}> = ({
+export const TableOfContents: FC<{ elements: { level: number; title: string }[]; pageFilePath?: string; pageTitle?: string }> = ({
   elements,
   pageFilePath,
   pageTitle
 }) => {
-  const [activeHeading, setActiveHeading] = useState('')
+  const [activeHeading, setActiveHeading] = useState("")
   const [showScrollToTopButton, setShowScrollToTopButton] = useState<boolean>(false)
 
   useEffect(() => {
     const handleScroll = () => {
-      let current = ''
-      for (const {title} of elements) {
+      let current = ""
+      for (const { title } of elements) {
         const slug = sluggifyTitle(getNodeText(title))
         const element = document.getElementById(slug)
         if (element && element.getBoundingClientRect().top < 256) current = slug
@@ -26,9 +26,9 @@ export const TableOfContents: FC<{elements: {level: number; title: string}[]; pa
       setShowScrollToTopButton(window.scrollY > 500)
     }
     handleScroll()
-    window.addEventListener('scroll', handleScroll, {passive: true})
+    window.addEventListener("scroll", handleScroll, { passive: true })
     return () => {
-      window.removeEventListener('scroll', handleScroll)
+      window.removeEventListener("scroll", handleScroll)
     }
   }, [elements])
 
@@ -36,25 +36,24 @@ export const TableOfContents: FC<{elements: {level: number; title: string}[]; pa
     const element = document.getElementById(slug)
     window.scrollTo({
       top: element!.getBoundingClientRect().top + window.scrollY - 128,
-      behavior: 'smooth'
+      behavior: "smooth"
     })
   }
 
   return (
-    <aside className="shrink-0 sticky top-32 sm:top-40 mb-16 w-40 shrink-0">
+    <aside className="toc shrink-0 sticky top-32 sm:top-40 mb-16 w-40">
       <div>
         {elements.length > 1 && <h2 className="text-white uppercase text-sm font-semibold h-8 flex items-end">On this page</h2>}
         <ul className="relative grow overflow-y-auto py-9 text-sm">
-          {elements.slice(1, elements.length).map(({level, title}, index) => {
+          {elements.slice(1, elements.length).map(({ level, title }, index) => {
             const slug = sluggifyTitle(getNodeText(title))
             return (
-              <li key={index} className="mt-2.5" style={{paddingLeft: `${level > 1 ? level - 2 : 0}rem`}}>
+              <li key={index} className="mt-2.5" style={{ paddingLeft: `${level > 1 ? level - 2 : 0}rem` }}>
                 <button
                   onClick={() => scrollTo(slug)}
-                  className={`flex items-center pb-1 hover:text-white leading-snug text-left ${slug === activeHeading ? 'text-white' : ''}`}
-                >
-                  {title}
-                </button>
+                  className={`flex items-center pb-1 hover:text-white leading-snug text-left ${slug === activeHeading ? "text-white" : ""}`}
+                  dangerouslySetInnerHTML={{ __html: title.replace("`", "<code>").replace("`", "</code>") }}
+                />
               </li>
             )
           })}
@@ -85,7 +84,7 @@ export const TableOfContents: FC<{elements: {level: number; title: string}[]; pa
               </Link>
             )}
             {showScrollToTopButton && (
-              <button onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})} className="mb-1.5 hover:text-white flex items-start gap-1">
+              <button onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })} className="mb-1.5 hover:text-white flex items-start gap-1">
                 <span>Scroll to top</span>
                 <Icon name="arrow-right" className="h-2.5 -rotate-90" />
               </button>
