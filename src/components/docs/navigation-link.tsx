04b8cf1338b791434458b49ec2695bdb8fb4adec
Lukas Wiesehan <mail@lukaswiesehan.de>
2023-12-13 11:34:26 +0100

    Tweak docs navigation

--- src/components/docs/navigation-link.tsx
+++ src/components/docs/navigation-link.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client"
 
-import Link from 'next/link'
-import {usePathname} from 'next/navigation'
-import {FC, useEffect, useState} from 'react'
-import {Icon} from '../icons'
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { FC, useEffect, useState } from "react"
+import { Icon } from "../icons"
 
-export const NavigationLink: FC<{level: number; element: DocsNavElement}> = ({level, element}) => {
+export const NavigationLink: FC<{ level: number; element: DocsNavElement }> = ({ level, element }) => {
   const pathname = usePathname()
   const [collapsed, setCollapsed] = useState<boolean>(element.collapsible ? (pathname.startsWith(element.urlPath) ? false : true) : false)
 
@@ -17,22 +17,23 @@ export const NavigationLink: FC<{level: number; element: DocsNavElement}> = ({le
     <li>
       <Link
         href={element.urlPath}
-        className={`h-5 flex items-center w-full justify-between px-4 border-l ${
-          element.urlPath === pathname ? 'border-white text-white' : 'border-transparent text-zinc-400'
-        } ${level === 0 && element.children.length ? 'text-white uppercase text-sm font-semibold mt-12' : 'mt-3'}`}
+        className={`leading-tight flex items-center w-full justify-between px-4 border-l ${
+          element.urlPath === pathname ? "border-white text-white" : "border-transparent text-zinc-400"
+        } ${level === 0 && element.children.length ? "text-white uppercase text-sm font-semibold mt-12" : "mt-3"}`}
+        style={{ minHeight: "1.25rem" }}
       >
-        <span className={`${level === 0 && element.children.length ? 'text-white' : ''}`} style={{marginLeft: `${level > 0 ? level - 1 : 0}rem`}}>
+        <span className={`${level === 0 && element.children.length ? "text-white" : ""}`} style={{ marginLeft: `${level > 0 ? level - 1 : 0}rem` }}>
           {element.title}
         </span>
         {element.children.length > 0 && element.collapsible && (
           <button
-            className="h-6 w-6 flex items-center justify-center"
+            className="h-5 w-5 flex items-center justify-center"
             onClick={(e) => {
               e.preventDefault()
               setCollapsed((collapsed) => !collapsed)
             }}
           >
-            <Icon name="chevron-right" className={`h-3 -mt-0.5 transition-transform duration-200 ${collapsed ? 'rotate-0' : 'rotate-90'}`} />
+            <Icon name="chevron-right" className={`h-3 -mt-0.5 transition-transform duration-200 ${collapsed ? "rotate-0" : "rotate-90"}`} />
           </button>
         )}
       </Link>
