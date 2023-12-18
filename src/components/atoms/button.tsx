513218b3e10eb0552c1aa30a91d296e5a3791689
Lukas Wiesehan <mail@lukaswiesehan.de>
2023-12-18 10:12:38 +0100

    Fix button overflow

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -1,27 +1,34 @@
-import Link from 'next/link'
-import {FC, ReactNode} from 'react'
-import {Icon} from '../icons'
+import Link from "next/link"
+import { FC, ReactNode } from "react"
+import { Icon } from "../icons"
 
-export const Button: FC<{secondary?: boolean | false; href: string; children: ReactNode; className?: string}> = ({
-  secondary,
-  href,
-  children,
-  className = ''
-}) => {
+export const Button: FC<{
+  secondary?: boolean | false
+  href: string
+  children: ReactNode
+  className?: string
+}> = ({ secondary, href, children, className = "" }) => {
   return (
     <Link
       href={href}
       className={`inline-flex relative z-10 h-10 rounded-xl p-px shadow-lg ${
-        secondary ? 'bg-gradient-to-br from-zinc-300 to-zinc-500' : 'bg-gradient-to-b from-white to-zinc-300'
+        secondary
+          ? "bg-gradient-to-br from-zinc-300 to-zinc-500"
+          : "bg-gradient-to-b from-white to-zinc-300"
       } ${className}`}
     >
       <div
-        className={`flex items-center gap-1 px-6 font-medium rounded-[11px] ${
-          secondary ? 'bg-gradient-to-br from-zinc-700 to-zinc-900 text-white' : 'bg-white text-black'
+        className={`flex items-center gap-1 px-6 font-medium rounded-[11px] whitespace-nowrap ${
+          secondary
+            ? "bg-gradient-to-br from-zinc-700 to-zinc-900 text-white"
+            : "bg-white text-black"
         }`}
       >
         <span>{children}</span>
-        <Icon name={href.startsWith('http') ? 'arrow-up-right' : 'arrow-right'} className={`h-3.5 ${href.startsWith('http') ? '-mt-1' : ''}`} />
+        <Icon
+          name={href.startsWith("http") ? "arrow-up-right" : "arrow-right"}
+          className={`h-3.5 ${href.startsWith("http") ? "-mt-1" : ""}`}
+        />
       </div>
     </Link>
   )
