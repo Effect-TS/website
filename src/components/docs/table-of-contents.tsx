fd2ebffe72e07a76b53383819f5bd512e3eee096
Lukas Wiesehan <mail@lukaswiesehan.de>
2023-12-18 11:49:44 +0100

    Fix toc overflow

--- src/components/docs/table-of-contents.tsx
+++ src/components/docs/table-of-contents.tsx
@@ -6,13 +6,14 @@ import { FC, useEffect, useState } from "react"
 import { Icon } from "../icons"
 import { Divider } from "../layout/divider"
 
-export const TableOfContents: FC<{ elements: { level: number; title: string }[]; pageFilePath?: string; pageTitle?: string }> = ({
-  elements,
-  pageFilePath,
-  pageTitle
-}) => {
+export const TableOfContents: FC<{
+  elements: { level: number; title: string }[]
+  pageFilePath?: string
+  pageTitle?: string
+}> = ({ elements, pageFilePath, pageTitle }) => {
   const [activeHeading, setActiveHeading] = useState("")
-  const [showScrollToTopButton, setShowScrollToTopButton] = useState<boolean>(false)
+  const [showScrollToTopButton, setShowScrollToTopButton] =
+    useState<boolean>(false)
 
   useEffect(() => {
     const handleScroll = () => {
@@ -20,7 +21,8 @@ export const TableOfContents: FC<{ elements: { level: number; title: string }[];
       for (const { title } of elements) {
         const slug = sluggifyTitle(getNodeText(title))
         const element = document.getElementById(slug)
-        if (element && element.getBoundingClientRect().top < 256) current = slug
+        if (element && element.getBoundingClientRect().top < 256)
+          current = slug
       }
       setActiveHeading(current)
       setShowScrollToTopButton(window.scrollY > 500)
@@ -43,29 +45,43 @@ export const TableOfContents: FC<{ elements: { level: number; title: string }[];
   return (
     <aside className="toc flex-none sticky top-32 sm:top-40 mb-16 w-40 overflow-y-auto hidden xl:block">
       <div>
-        {elements.length > 1 && <h2 className="text-black dark:text-white uppercase text-sm font-semibold h-8 flex items-end">On this page</h2>}
+        {elements.length > 1 && (
+          <h2 className="text-black dark:text-white uppercase text-sm font-semibold h-8 flex items-end">
+            On this page
+          </h2>
+        )}
         <ul className="relative grow overflow-y-auto py-9 text-sm">
-          {elements.slice(1, elements.length).map(({ level, title }, index) => {
-            const slug = sluggifyTitle(getNodeText(title))
-            return (
-              <li key={index} className="mt-2.5" style={{ paddingLeft: `${level > 1 ? level - 2 : 0}rem` }}>
-                <button
-                  onClick={() => scrollTo(slug)}
-                  className={`flex items-center pb-1 hover:text-black dark:hover:text-white leading-snug text-left ${
-                    slug === activeHeading ? "text-black font-normal dark:text-white dark:font-light" : ""
-                  }`}
-                  dangerouslySetInnerHTML={{ __html: title.replace("`", "<code>").replace("`", "</code>") }}
-                />
-              </li>
-            )
-          })}
+          {elements
+            .slice(1, elements.length)
+            .map(({ level, title }, index) => {
+              const slug = sluggifyTitle(getNodeText(title))
+              return (
+                <li
+                  key={index}
+                  className="mt-2.5"
+                  style={{ paddingLeft: `${level > 1 ? level - 2 : 0}rem` }}
+                >
+                  <button
+                    onClick={() => scrollTo(slug)}
+                    className={`flex items-center pb-1 hover:text-black dark:hover:text-white leading-snug text-left ${
+                      slug === activeHeading
+                        ? "text-black font-normal dark:text-white dark:font-light"
+                        : ""
+                    }`}
+                    dangerouslySetInnerHTML={{
+                      __html: title
+                        .replace("`", "<code>")
+                        .replace("`", "</code>")
+                    }}
+                  />
+                </li>
+              )
+            })}
         </ul>
       </div>
       {elements.length > 1 && (
         <>
-          <div className="-mx-8">
-            <Divider />
-          </div>
+          <Divider />
           <div className="text-sm py-9 space-y-3">
             {pageFilePath && (
               <Link
@@ -87,7 +103,9 @@ export const TableOfContents: FC<{ elements: { level: number; title: string }[];
             )}
             {showScrollToTopButton && (
               <button
-                onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+                onClick={() =>
+                  window.scrollTo({ top: 0, behavior: "smooth" })
+                }
                 className="mb-1.5 hover:text-black dark:hover:text-white flex items-start gap-1"
               >
                 <span>Scroll to top</span>
