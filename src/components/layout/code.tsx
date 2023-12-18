cddecd3f5af50c8b33881c0b1c49dff91a91b285
Lukas Wiesehan <mail@lukaswiesehan.de>
2023-12-18 10:17:12 +0100

    Tweak code examples window

--- src/components/layout/code.tsx
+++ src/components/layout/code.tsx
@@ -1,15 +1,19 @@
-'use client'
+"use client"
 
-import {FC, useState} from 'react'
-import * as Tabs from '@radix-ui/react-tabs'
-import hljs from 'highlight.js/lib/common'
-import {Icon} from '../icons'
+import { FC, useState } from "react"
+import * as Tabs from "@radix-ui/react-tabs"
+import hljs from "highlight.js/lib/common"
+import { Icon } from "../icons"
 
 export const Code: FC<{
-  tabs: {name: string; content: string; highlights?: {color: string; lines: number[]}[]}[]
-  terminal?: {run: string; command: string; result: string}
+  tabs: {
+    name: string
+    content: string
+    highlights?: { color: string; lines: number[] }[]
+  }[]
+  terminal?: { run: string; command: string; result: string }
   fixedHeight?: 80 | 96
-}> = ({tabs, terminal, fixedHeight}) => {
+}> = ({ tabs, terminal, fixedHeight }) => {
   const [running, setRunning] = useState<boolean>(false)
   const runSnippet = () => {
     setRunning(true)
@@ -17,59 +21,86 @@ export const Code: FC<{
   }
 
   return (
-    <div className="w-full bg-gradient-to-br from-zinc-500 to-zinc-800 p-px rounded-xl overflow-hidden">
-      <Tabs.Root defaultValue={tabs[0].name} className="bg-zinc-950 rounded-[11px] overflow-hidden">
-        <div className="flex border-b border-zinc-800">
-          <div className="h-10 flex items-center gap-2 px-3 border-r border-zinc-800">
-            <div className="h-3 w-3 rounded-full bg-zinc-700" />
-            <div className="h-3 w-3 rounded-full bg-zinc-700" />
-            <div className="h-3 w-3 rounded-full bg-zinc-700" />
+    <div className="w-full bg-gradient-to-br from-zinc-500 to-zinc-800 p-px rounded-xl overflow-hidden h-full">
+      <Tabs.Root
+        defaultValue={tabs[0].name}
+        className="bg-zinc-950 rounded-[11px] overflow-hidden h-full flex flex-col justify-between"
+      >
+        <div>
+          <div className="flex border-b border-zinc-800">
+            <div className="h-10 flex items-center gap-2 px-3 border-r border-zinc-800">
+              <div className="h-3 w-3 rounded-full bg-zinc-700" />
+              <div className="h-3 w-3 rounded-full bg-zinc-700" />
+              <div className="h-3 w-3 rounded-full bg-zinc-700" />
+            </div>
+            <Tabs.List className="flex">
+              {tabs.map(({ name }, index) => (
+                <Tabs.Trigger
+                  key={index}
+                  value={name}
+                  className="h-10 px-3 flex items-center font-mono text-xs border-r border-zinc-800 data-[state=active]:text-white"
+                >
+                  {name}
+                </Tabs.Trigger>
+              ))}
+            </Tabs.List>
           </div>
-          <Tabs.List className="flex">
-            {tabs.map(({name}, index) => (
-              <Tabs.Trigger
-                key={index}
-                value={name}
-                className="h-10 px-3 flex items-center font-mono text-xs border-r border-zinc-800 data-[state=active]:text-white"
-              >
-                {name}
-              </Tabs.Trigger>
-            ))}
-          </Tabs.List>
-        </div>
-        <div className={`${terminal ? 'pb-8' : ''} ${fixedHeight === 80 ? 'lg:h-80' : fixedHeight === 96 ? 'lg:h-96' : ''} max-h-96 overflow-y-auto`}>
-          {tabs.map(({name, content, highlights}, index) => {
-            const html = hljs.highlightAuto(content).value
-            const lineCount = (html.match(/\n/g) || []).length + 1
-            return (
-              <Tabs.Content key={index} value={name} className="relative font-mono text-sm flex data-[state=inactive]:absolute">
-                <ul className="absolute inset-0 text-zinc-600 py-3">
-                  {[...new Array(lineCount)].map((e, i) => {
-                    let backgroundColor = 'transparent'
-                    highlights?.forEach((highlight) => {
-                      if (highlight.lines.includes(i + 1)) {
-                        backgroundColor = highlight.color
-                      }
-                    })
-                    return (
-                      <li key={i} className="relative">
-                        <div className="absolute inset-y-0 left-0 w-full" style={{backgroundColor}} />
-                        <span className="relative block w-12 pr-4 text-right">{i + 1}</span>
-                      </li>
-                    )
-                  })}
-                </ul>
-                <div className="relative w-full py-3 pl-12">
-                  <div className="w-full overflow-x-auto ">
-                    {/* `contain: none` needed for horizontal scrolling to work */}
-                    <pre style={{contain: 'none'}}>
-                      <code dangerouslySetInnerHTML={{__html: html.replaceAll('\n', '<br/>')}} className="block" />
-                    </pre>
+          <div
+            className={`${terminal ? "pb-8" : ""} ${
+              fixedHeight === 80
+                ? "lg:h-80"
+                : fixedHeight === 96
+                  ? "lg:h-96"
+                  : ""
+            } max-h-96 overflow-y-auto`}
+          >
+            {tabs.map(({ name, content, highlights }, index) => {
+              const html = hljs.highlightAuto(content).value
+              const lineCount = (html.match(/\n/g) || []).length + 1
+              return (
+                <Tabs.Content
+                  key={index}
+                  value={name}
+                  className="relative font-mono text-sm flex data-[state=inactive]:absolute"
+                >
+                  <ul className="absolute inset-0 text-zinc-600 py-3">
+                    {[...new Array(lineCount)].map((e, i) => {
+                      let backgroundColor = "transparent"
+                      highlights?.forEach((highlight) => {
+                        if (highlight.lines.includes(i + 1)) {
+                          backgroundColor = highlight.color
+                        }
+                      })
+                      return (
+                        <li key={i} className="relative">
+                          <div
+                            className="absolute inset-y-0 left-0 w-full"
+                            style={{ backgroundColor }}
+                          />
+                          <span className="relative block w-12 pr-4 text-right">
+                            {i + 1}
+                          </span>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                  <div className="relative w-full py-3 pl-12">
+                    <div className="w-full overflow-x-auto ">
+                      {/* `contain: none` needed for horizontal scrolling to work */}
+                      <pre style={{ contain: "none" }}>
+                        <code
+                          dangerouslySetInnerHTML={{
+                            __html: html.replaceAll("\n", "<br/>")
+                          }}
+                          className="block"
+                        />
+                      </pre>
+                    </div>
                   </div>
-                </div>
-              </Tabs.Content>
-            )
-          })}
+                </Tabs.Content>
+              )
+            })}
+          </div>
         </div>
         {terminal && (
           <div className="relative border-t border-zinc-800 py-8">
@@ -83,11 +114,13 @@ export const Code: FC<{
               </div>
             </button>
             <div className="font-mono text-white text-sm flex py-3 overflow-x-auto">
-              <div className="text-zinc-600 w-12 shrink-0 text-right pr-4">$</div>
+              <div className="text-zinc-600 w-12 shrink-0 text-right pr-4">
+                $
+              </div>
               <div>
                 <div>{terminal.command}</div>
                 <div className="relative">
-                  <pre className={`${running ? 'opacity-0' : ''}`}>
+                  <pre className={`${running ? "opacity-0" : ""}`}>
                     <code>
                       <br />
                       {terminal.result}
