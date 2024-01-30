9db8d4adfa5998bfc02a8ebac6ec957dcff92ec2
Lukas Wiesehan <mail@lukaswiesehan.de>
2024-01-30 11:58:21 +0100

    Only show copy button when hovering a code snippet

--- src/components/docs/components/pre.tsx
+++ src/components/docs/components/pre.tsx
@@ -19,9 +19,9 @@ export const Pre: FC<PropsWithChildren<{ filename?: string }>> = ({
 
   return (
     <div className="relative mt-7">
-      <div className="w-full bg-gradient-to-br from-zinc-500 to-zinc-800 p-px rounded-xl overflow-hidden [.code-output_&]:rounded-b-none">
+      <div className="group w-full bg-gradient-to-br from-zinc-500 to-zinc-800 p-px rounded-xl overflow-hidden [.code-output_&]:rounded-b-none">
         <div className="bg-zinc-950 rounded-[11px] [.code-output_&]:rounded-b-none">
-          <div className="flex justify-between items-center pr-2 border-b border-zinc-800">
+          <div className="flex justify-start group-hover:justify-between items-center pr-2 border-b border-zinc-800">
             <div className="flex">
               <div className="h-10 flex items-center gap-2 px-3 border-r border-zinc-800">
                 <div className="h-3 w-3 rounded-full bg-zinc-700" />
@@ -29,14 +29,14 @@ export const Pre: FC<PropsWithChildren<{ filename?: string }>> = ({
                 <div className="h-3 w-3 rounded-full bg-zinc-700" />
               </div>
               {filename && (
-                <div className="h-10 px-3 flex items-center border-r border-zinc-800 font-mono text-sm text-zinc-800 dark:text-zinc-300">
+                <div className="h-10 px-3 flex items-center border-r border-zinc-800 font-mono text-sm text-zinc-300 dark:text-zinc-300">
                   {filename}
                 </div>
               )}
             </div>
             <button
               onClick={copy}
-              className="h-6 rounded-lg p-px shadow-lg bg-gradient-to-br from-zinc-300 to-zinc-500"
+              className="hidden group-hover:block h-6 rounded-lg p-px shadow-lg bg-gradient-to-br from-zinc-300 to-zinc-500"
             >
               <div className="h-full flex items-center gap-1 px-2 font-medium rounded-[7px] bg-gradient-to-br from-zinc-700 to-zinc-900 text-white text-sm">
                 <Icon name={copied ? "check" : "clipboard"} className="h-3" />
