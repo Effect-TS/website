d8c5e9d2121b55fa5051fbce35a1b89d6ce96c29
Tim <hello@timsmart.co>
2024-05-08 15:41:31 +1200

    nav chevron

--- src/app/tutorials/[...slug]/components/Navigation.tsx
+++ src/app/tutorials/[...slug]/components/Navigation.tsx
@@ -22,14 +22,16 @@ export async function Navigation({
       >
         <Icon name="arrow-right" className="h-full rotate-180" />
       </Link>
-      <nav className="bg-gray-100 dark:bg-gray-900 dark:border-gray-600 p-2 rounded border flex-1 mx-2">
+      <nav className="bg-gray-100 dark:bg-gray-900 dark:border-gray-600 p-2 rounded border flex-1 mx-2 flex items-center cursor-pointer">
         <strong className="font-medium">{tutorial.section}</strong>
         <Separator />
         <span>{tutorial.title}</span>
+        <div className="flex-1" aria-hidden />
+        <Icon name="chevron-right" className="h-4 rotate-90" />
       </nav>
       <Link
         href={next?.urlPath ?? "#"}
-        className={`h-4 ${next ? "" : "text-gray-300"}`}
+        className={`h-4 ${next ? "" : "text-gray-400"}`}
       >
         <Icon name="arrow-right" className="h-full" />
       </Link>
@@ -38,5 +40,5 @@ export async function Navigation({
 }
 
 function Separator() {
-  return <span className="text-lg p-2">/</span>
+  return <span className="text-lg px-2 leading-none">/</span>
 }
