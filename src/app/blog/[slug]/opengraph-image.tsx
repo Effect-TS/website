b5c958dd8d4383086236c761e3fd57a2d30df5f5
Tim <hello@timsmart.co>
2024-05-15 10:23:22 +1200

    reduce size of opengraph-image bundle (#630)

--- src/app/blog/[slug]/opengraph-image.tsx
+++ src/app/blog/[slug]/opengraph-image.tsx
@@ -1,36 +1,46 @@
-import {allBlogPosts} from 'contentlayer/generated'
-import {ImageResponse} from 'next/og'
+import { ImageResponse } from "next/og"
 
-export const runtime = 'edge'
-export const alt = 'Effect'
+export const runtime = "edge"
+export const alt = "Effect"
 export const size = {
   width: 1200,
   height: 630
 }
 
-export const contentType = 'image/png'
+export const contentType = "image/png"
 
-export default async function Image({params: {slug}}: {params: {slug: string}}) {
+export default async function Image({
+  params: { slug }
+}: {
+  params: { slug: string }
+}) {
+  const { allBlogPosts } = await import("contentlayer/generated")
   const post = allBlogPosts.find((post) => post.urlPath === `/blog/${slug}`)!
 
   return new ImageResponse(
     (
       <div
         style={{
-          backgroundImage: 'url(https://i.imgur.com/iBLRVtL.jpeg)',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          gap: '64px',
-          flexDirection: 'column',
-          alignItems: 'flex-start',
-          justifyContent: 'center',
-          padding: '0px 128px'
+          backgroundImage: "url(https://i.imgur.com/iBLRVtL.jpeg)",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          gap: "64px",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          justifyContent: "center",
+          padding: "0px 128px"
         }}
       >
-        <img src="https://i.imgur.com/aGMJKsB.png" alt="Effect Logotype" height="64" />
-        <div style={{color: 'white', fontSize: 64, fontFamily: 'CalSans'}}>{post.title}</div>
-        <div style={{color: '#A1A1AB', fontSize: 32}}>{post.excerpt}</div>
+        <img
+          src="https://i.imgur.com/aGMJKsB.png"
+          alt="Effect Logotype"
+          height="64"
+        />
+        <div style={{ color: "white", fontSize: 64, fontFamily: "CalSans" }}>
+          {post.title}
+        </div>
+        <div style={{ color: "#A1A1AB", fontSize: 32 }}>{post.excerpt}</div>
         {/* <div style={{fontFamily: 'Cal Sans', color: '#ffffff', fontSize: 64}}>Test</div> */}
       </div>
     ),
@@ -38,15 +48,19 @@ export default async function Image({params: {slug}}: {params: {slug: string}}) 
       ...size,
       fonts: [
         {
-          name: 'Inter',
-          data: await fetch(new URL('../../inter-light.ttf', import.meta.url)).then((res) => res.arrayBuffer()),
-          style: 'normal',
+          name: "Inter",
+          data: await fetch(
+            new URL("../../inter-light.ttf", import.meta.url)
+          ).then((res) => res.arrayBuffer()),
+          style: "normal",
           weight: 300
         },
         {
-          name: 'CalSans',
-          data: await fetch(new URL('../../cal-sans-semibold.ttf', import.meta.url)).then((res) => res.arrayBuffer()),
-          style: 'normal',
+          name: "CalSans",
+          data: await fetch(
+            new URL("../../cal-sans-semibold.ttf", import.meta.url)
+          ).then((res) => res.arrayBuffer()),
+          style: "normal",
           weight: 600
         }
       ]
