35a3f842b667828ed331a832c80207b6ee9e0afb
Tim <hello@timsmart.co>
2024-05-16 11:54:26 +1200

    add expanding groups to tutorial menu

--- src/app/tutorials/[...slug]/components/Navigation/MenuButton.tsx
+++ src/app/tutorials/[...slug]/components/Navigation/MenuButton.tsx
@@ -31,14 +31,15 @@ export function MenuButton({
     <div className="flex-1 bg-gradient-to-br from-zinc-200 to-zinc-300 dark:from-zinc-600 dark:to-zinc-900 rounded-lg p-px cursor-pointer">
       <nav
         ref={ref}
-        className="bg-gray-100 dark:bg-neutral-900 p-2 rounded-lg flex items-center relative"
-        onClick={onClick}
+        className="bg-gray-100 dark:bg-neutral-900 p-2 rounded-lg relative"
       >
-        <strong className="font-medium">{section}</strong>
-        <Separator />
-        <span>{title}</span>
-        <div className="flex-1" aria-hidden />
-        <Icon name="chevron-right" className="h-4 rotate-90" />
+        <div onClick={onClick} className="flex items-center">
+          <strong className="font-medium">{section}</strong>
+          <Separator />
+          <span>{title}</span>
+          <div className="flex-1" aria-hidden />
+          <Icon name="chevron-right" className="h-4 rotate-90" />
+        </div>
         <div
           className={`absolute transition-all z-10 top-0 left-0 w-full mt-12 ${
             visible ? "visible opacity-100" : "invisible opacity-0"
--- src/app/tutorials/grouped.ts
+++ src/app/tutorials/grouped.ts
@@ -36,3 +36,5 @@ export const groupedTutorials = (function () {
   }
   return grouped
 })()
+
+export type TutorialGroup = (typeof groupedTutorials)[string]
