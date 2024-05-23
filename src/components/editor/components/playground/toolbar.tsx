832307a79ed5cc0ceb2b65f43ac871b56445f383
Tim <hello@timsmart.co>
2024-05-23 14:48:24 +1200

    remove Presets

--- src/components/editor/components/playground/toolbar.tsx
+++ src/components/editor/components/playground/toolbar.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { ShareButton } from "./share-button"
-import { PresetSelector } from "./preset-selector"
 
 export declare namespace Toolbar {
   export interface Props {}
@@ -12,18 +11,6 @@ export const Toolbar: React.FC<Toolbar.Props> = () => {
       <ToolbarHeader />
 
       <div className="w-full ml-auto flex space-x-2 sm:justify-end">
-        <PresetSelector
-          presets={[
-            {
-              id: "9cb0e66a-9937-465d-a188-2c4c4ae2401f",
-              name: "Basic CLI"
-            },
-            {
-              id: "61eb0e32-2391-4cd3-adc3-66efe09bc0b7",
-              name: "Http Server"
-            }
-          ]}
-        />
         <div className="hidden space-x-2 md:flex">
           <ShareButton />
         </div>
@@ -48,3 +35,20 @@ const ToolbarHeader: React.FC<ToolbarHeader.Props> = () => {
     </h2>
   )
 }
+
+// function Presets() {
+//   return (
+//     <PresetSelector
+//       presets={[
+//         {
+//           id: "9cb0e66a-9937-465d-a188-2c4c4ae2401f",
+//           name: "Basic CLI"
+//         },
+//         {
+//           id: "61eb0e32-2391-4cd3-adc3-66efe09bc0b7",
+//           name: "Http Server"
+//         }
+//       ]}
+//     />
+//   )
+// }
