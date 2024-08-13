f53873d6d2df5b1751663925c8ef1a71a8ed50e8
jk <47693+sectore@users.noreply.github.com>
2024-08-13 17:07:24 +0200

    fix (flake): use same node version for corepack (#797)

--- flake.nix
+++ flake.nix
@@ -3,36 +3,22 @@
     nixpkgs = {
       url = "github:nixos/nixpkgs/nixpkgs-unstable";
     };
-
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      corepackEnable = pkgs.runCommand "corepack-enable" {} ''
-        mkdir -p $out/bin
-        ${pkgs.nodejs-18_x}/bin/corepack enable --install-directory $out/bin
-      '';
+  outputs = {nixpkgs, ...}: let 
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed
+      (system: function nixpkgs.legacyPackages.${system});
     in {
-      formatter = pkgs.alejandra;
+      formatter = forAllSystems (pkgs: pkgs.alejandra);
 
-      devShells = {
+      devShells = forAllSystems(pkgs: {
         default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            bun
+            corepack
             nodejs_20
-            corepackEnable
-            nodePackages.json
           ];
         };
-      };
-    });
+      });
+    };
 }
