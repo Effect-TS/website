2cbd9b2d17fab9d52c28ba2dc39e048e1eddf1d3
Maxwell Brown <maxwellbrown1990@gmail.com>
2024-12-01 13:20:22 -0500

    fix edit docs link

--- content/astro.config.ts
+++ content/astro.config.ts
@@ -134,7 +134,7 @@ export default defineConfig({
         "./src/styles/twoslash.css"
       ],
       editLink: {
-        baseUrl: "https://github.com/Effect-TS/website/edit/main/docs/"
+        baseUrl: "https://github.com/Effect-TS/website/blob/main/content/src/content/docs/docs/"
       },
       expressiveCode: {
         plugins: [
