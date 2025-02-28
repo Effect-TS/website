868456947a1bfdd75f937322494b8fa87e298cbc
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-02-28 17:10:06 -0500

    prerender

--- content/src/pages/open-graph/[...path].ts
+++ content/src/pages/open-graph/[...path].ts
@@ -1,6 +1,8 @@
 import { getCollection } from "astro:content"
 import { OGImageRoute } from "astro-og-canvas"
 
+export const prerender = true
+
 const entries = await getCollection("docs")
 
 const pages = Object.fromEntries(entries.map(({ data, id }) => [id, { data }]))
