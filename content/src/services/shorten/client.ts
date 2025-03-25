4e67d0b0081ffdb1352b43bd8f84f91d66242fb0
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-03-25 15:17:24 -0400

    update to latest rpc api

--- content/src/services/shorten/client.ts
+++ content/src/services/shorten/client.ts
@@ -1,4 +1,12 @@
-import type { RpcRouter } from "@/pages/api/rpc"
-import * as Resolver from "@effect/rpc-http/HttpRpcResolverNoStream"
+import * as Layer from "effect/Layer"
+import * as FetchHttpClient from "@effect/platform/FetchHttpClient"
+import * as RpcSerialization from "@effect/rpc/RpcSerialization"
+import * as RpcClient from "@effect/rpc/RpcClient"
 
-export const rpcClient = Resolver.makeClient<RpcRouter>("/api/rpc/")
+export const ShortenClientLayer = RpcClient.layerProtocolHttp({
+  url: "/api/rpc/"
+}).pipe(Layer.provide([
+  FetchHttpClient.layer,
+  RpcSerialization.layerJson
+]))
+
