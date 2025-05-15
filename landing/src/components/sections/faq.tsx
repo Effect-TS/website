6278ead3e1afa4970bdcca21ed270fe892464579
Mark Jaquith <mark@jaquith.me>
2025-05-15 00:12:45 -0400

    Improve wording, fix spelling and punctuation issues and inconsistency

--- landing/src/components/sections/faq.tsx
+++ landing/src/components/sections/faq.tsx
@@ -4,27 +4,27 @@ import { Divider } from "../layout/divider"
 const questions = [
   {
     q: "Can I incrementally adopt Effect?",
-    a: "Yes! Adopting Effect you can start by refactoring small portions of your app, usually the ones with higher complexity, and keep going as you see fit",
+    a: "Yes! When adopting Effect you can start by refactoring small portions of your app, usually the ones with higher complexity, and keep going as you see fit.",
     link: "/docs"
   },
   {
     q: "Does Effect scale?",
-    a: "Effect was built for production since the very beginning, we take good care of making everything as performant as possible, by providing you with better ways to deal with concurrency and great observability finding bottlenecks in your program becomes easy",
+    a: "Effect was built for production from the start. We take good care of making everything as fast as possible. And by providing you with better ways to deal with concurrency and great observability, finding bottlenecks in your program becomes easy.",
     link: "/docs"
   },
   {
     q: "Do I have to know functional programming?",
-    a: "No! While Effect makes usage of Functional Programming principles and patterns internally you can be proficient in Effect by simply using it as a smart Promise and forget that there is even a thing called Functional Programming",
+    a: "No! While Effect makes usage of Functional Programming principles and patterns internally you can be proficient in Effect by simply using it as a smart Promise and forget that there is even a thing called Functional Programming.",
     link: "/docs"
   },
   {
     q: "The library is huge, do I have to know it all?",
-    a: "No! Every module in Effect is made with a specific problem in mind that is deemed to be common enough but you don't need to know everything, in fact you can harness 80% of the productivity gain by just learning a few functions and 2-3 core modules.",
+    a: "No! Every module in Effect is made with a specific problem in mind that is deemed to be common enough but you don't need to know everything. In fact you can harness 80% of the productivity gain by just learning a few functions and 2-3 core modules.",
     link: "/docs"
   },
   {
     q: "What's the minimum bundle size?",
-    a: "The core of Effect is a runtime system that weights about 15k when compressed and tree-shaken, the rest scales with usage. If you end up using 100k of Effect code there is a good chance your app would have been 1Mb if not using Effect",
+    a: "The core of Effect is a runtime system that weighs about 15k when compressed and tree-shaken; the rest scales with usage. If you end up using 100k of Effect code there is a good chance your app would have been 1Mb if not using Effect.",
     link: "/docs"
   }
 ]
