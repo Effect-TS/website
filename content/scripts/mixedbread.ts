13943310182342427b9a24821a98ae71a57a8972
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-08-08 21:11:52 -0400

    fix script

--- content/scripts/mixedbread.ts
+++ content/scripts/mixedbread.ts
@@ -2,33 +2,26 @@ import { Mixedbread, toFile } from "@mixedbread/sdk"
 import Glob from "fast-glob"
 import Path from "node:path"
 import Fs from "node:fs"
-import FrontMatter from "front-matter"
 
 const mxbai = new Mixedbread({
   apiKey: process.env.MXBAI_API_KEY
 })
 
-
 const cwd = process.cwd()
-const path = Path.join(cwd, "src/content/docs/docs/ai/**/*.mdx")
+const path = Path.join(cwd, "src/content/docs/docs/**/*.mdx")
 const filePaths = await Glob(path)
 
-interface Attributes {
-  readonly title?: string
-  readonly description?: string
-}
-
-const vectorStoreId = process.env.MXBAI_VECTOR_STORE_ID
+const vectorStoreId = process.env.MXBAI_VECTOR_STORE_ID!
 for (const filePath of filePaths) {
-  const file = await toFile(Fs.createReadStream(filePath))
-  const text = await file.text()
-  const frontmatter = FrontMatter<Attributes>(text)
+  const file = await toFile(
+    Fs.createReadStream(filePath),
+    Path.basename(filePath),
+    { type: "text/markdown" }
+  )
   const url = filePath.replace(Path.join(cwd, "src/content/docs"), "")
   await mxbai.vectorStores.files.upload(vectorStoreId, file, {
     metadata: {
-      title: frontmatter.attributes.title ?? "",
-      url: url.split(".")[0],
-      description: frontmatter.attributes.description ?? ""
+      urlPath: url.split(".")[0],
     }
   })
 }
