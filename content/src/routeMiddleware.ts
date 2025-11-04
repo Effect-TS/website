3489e1104ab39396ce8a03d05547716dcfbfcc19
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-11-04 08:43:55 -0500

    fix url

--- content/src/routeMiddleware.ts
+++ content/src/routeMiddleware.ts
@@ -4,7 +4,7 @@ export const onRequest = defineRouteMiddleware((context) => {
   // Get the URL of the generated image for the current page using its ID and
   // append the `.png` file extension.
   const ogImageUrl = new URL(
-    `/og/${(context.locals as any).starlightRoute.entry.slug || "index"}.png`,
+    `/open-graph/${(context.locals as any).starlightRoute.entry.slug || "index"}.png`,
     context.site,
   );
 
