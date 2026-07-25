6c13c3d9fc7f23f475a1c6424eddd7fb04a0a003
Maxwell Brown <maxwellbrown1990@gmail.com>
2026-07-25 12:17:37 -0400

    fix: use canonical playground RPC URL

--- src/services/shorten/client.ts
+++ src/services/shorten/client.ts
@@ -1,6 +1,8 @@
 import * as Context from "effect/Context"
 import * as Layer from "effect/Layer"
 import * as FetchHttpClient from "effect/unstable/http/FetchHttpClient"
+import * as HttpClient from "effect/unstable/http/HttpClient"
+import * as HttpClientRequest from "effect/unstable/http/HttpClientRequest"
 import * as RpcClient from "effect/unstable/rpc/RpcClient"
 import { RpcClientError } from "effect/unstable/rpc/RpcClientError"
 import * as RpcGroup from "effect/unstable/rpc/RpcGroup"
@@ -13,10 +15,10 @@ export class ShortenClient extends Context.Service<
 >()("app/ShortenClient") {
   static readonly layer = Layer.effect(ShortenClient, RpcClient.make(ShortenRpcs)).pipe(
     Layer.provide(
-      RpcClient.layerProtocolHttp({ url: "/api/rpc" }).pipe(
-        Layer.provide(FetchHttpClient.layer),
-        Layer.provide(RpcSerialization.layerJson),
-      ),
+      RpcClient.layerProtocolHttp({
+        url: "/api/rpc",
+        transformClient: HttpClient.mapRequest(HttpClientRequest.setUrl("/api/rpc")),
+      }).pipe(Layer.provide(FetchHttpClient.layer), Layer.provide(RpcSerialization.layerJson)),
     ),
   )
 }
