7dbb15b52cbeb7d8404c34359c250e6f47cf5fac
Federico Biccheddu <service+git@f15u.dev>
2026-08-06 23:24:11 +0200

    chore: add version switch back

--- astro.config.ts
+++ astro.config.ts
@@ -121,6 +121,10 @@ export default defineConfig({
       status: 307,
       destination: "/docs/v3/getting-started/introduction",
     },
+    "/docs/v4": {
+      status: 307,
+      destination: "/docs/v4/getting-started/introduction",
+    },
     "/docs/api": {
       status: 307,
       destination: "/docs/v4/api",
