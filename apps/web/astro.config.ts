d5195f51e50d424d44e6cab9d6fcfeb039b5d6ae
FedericoBiccheddu <service+git@f15u.dev>
2026-08-27 12:52:53 +0200

    chore: split content sections

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -146,13 +146,21 @@ const config = defineConfig({
     },
     "/docs": {
       status: 307,
-      destination: "/docs/v4/getting-started/introduction",
+      destination: "/docs/v4/onboarding",
     },
     "/docs/v3": {
       status: 307,
-      destination: "/docs/v3/getting-started/introduction",
+      destination: "/docs/v3/onboarding",
     },
     "/docs/v4": {
+      status: 307,
+      destination: "/docs/v4/onboarding",
+    },
+    "/docs/v3/guides": {
+      status: 307,
+      destination: "/docs/v3/getting-started/introduction",
+    },
+    "/docs/v4/guides": {
       status: 307,
       destination: "/docs/v4/getting-started/introduction",
     },
